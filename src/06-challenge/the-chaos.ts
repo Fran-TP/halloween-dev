62b653db9ee3977bea96d86d9a868154dff3a475
Fran Torres <ftorresp@unasam.edu.pe>
2025-11-21 02:14:12 -0500

    Inline modulo, remove wrap and use non-null assert

--- src/06-challenge/the-chaos.ts
+++ src/06-challenge/the-chaos.ts
@@ -1,18 +1,14 @@
 export default function surviveRoulette(n: number, k: number): number {
-  const wrap = (index: number, length: number): number => {
-    return index % length
-  }
-
   const victims = Array.from({ length: n }, (_, i) => i)
   let lastIdx = 0
 
   while (victims.length > 1) {
     lastIdx += k - 1
-    const victimIdx = wrap(lastIdx, victims.length)
+    const victimIdx = lastIdx % victims.length
 
     victims.splice(victimIdx, 1)
     lastIdx = victimIdx
   }
 
-  return Number(victims[0])
+  return victims[0]!
 }
